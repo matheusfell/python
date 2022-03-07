e6c0620beb951a11ed9fb323b4165bd46409ceeb
Matheus Fellipe <bentomatheus48@gmail.com>
2022-03-06 22:31:45 -0300

    teste

---
+++ teste1.py
@@ -0,0 +1,5 @@
+print('Olá este simples programa irá te ajudar a fazer soma :)')
+soma1 = int(input('Qual primeiro valor que deseja calcular ?'))
+soma2 = int(input('Qual segundo valor que deseja calcular ?'))
+valor = soma1 + soma2
+print (f'A soma de {soma1} com {soma2} é {valor} obrigado :D')
